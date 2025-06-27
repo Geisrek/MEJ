fe43fb1d4367be8f6323a572d922a500f23b50ab
Rafik Hijazy <rafikhz90@gmail.com>
2025-06-28 02:34:54 +0300

    update pakages 1

--- src/app/component/services/services.component.ts
+++ src/app/component/services/services.component.ts
@@ -1,11 +1,22 @@
 import { Component } from '@angular/core';
+import { GoogleMapsModule } from '@angular/google-maps';
+import { BrowserModule } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-services',
-  imports: [],
+  imports: [
+    GoogleMapsModule,
+    BrowserModule,
+    AgmCoreModule
+  ],
   templateUrl: './services.component.html',
   styleUrl: './services.component.css'
 })
 export class ServicesComponent {
+  center: google.maps.LatLngLiteral = { lat: 24, lng: 12 };
+  zoom = 4;
+   title = 'My first AGM project';
+  lat = 51.678418;
+  lng = 7.809007;
 
 }
